efbaa63735b1b9ff0e65c3c0bf549f6dfff927d8
scarlet <sarahscarlet641@gmail.com>
2024-06-09 20:23:13 +0300

    complete

--- lib/email_address_parser.py
+++ lib/email_address_parser.py
@@ -1,1 +1,14 @@
 # your code goes here!
+import re
+class EmailAddressParser:
+    def __init__(self, addresses):
+        self.addresses = addresses
+
+    def parse(self):
+        # Split the string by commas and spaces
+        split_addresses =re.split(r'[,\s]+', self.addresses)
+
+        # Filter out empty strings and whitespace-only entries
+        split_addresses = [address.strip() for address in split_addresses if address.strip()]
+
+        return split_addresses
